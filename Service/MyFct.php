73f72cad26232fdb426c5f5dddbe89fa924c2b3e
GoT <timothee.gobert92@gmail.com>
2024-01-16 15:57:07 +0100

    supp

--- Service/MyFct.php
+++ Service/MyFct.php
@@ -4,24 +4,18 @@ require_once("./Config/parametre.php");
 
 class MyFct{
 
-    function generatePage($file,$variables=[],$base="View/base-bs.html.php"){  // generation d'une page
-        // $file  : fichier html
-        //$variables  : une variable en tableau qui contnient comme indices les noms des variables utilisées par $file
-        //Exemple ['x'=>2,'y'=>5,'z'=>10]   . avec extract($variables) , on a $x=2;  $y=5 et $z=10
-        if(file_exists($file)){   // if faut verifier si le $file existe ou non
-            //cas de $file existe
-            extract($variables);
-            ob_start();   // Ouvrir   la memoire tampon pour contenir lfichier $file à transformer en texte
+    function generatePage($file,$variables=[],$base="View/base-bs.html.php"){
+        //$variables  : une variable en tableau qui contnient comme indices les noms des variables utilisées par $file  
+        if(file_exists($file)){
+            extract($variables);    // Exemple ['x'=>2,'y'=>5,'z'=>10]   . avec extract($variables) , on a $x=2;  $y=5 et $z=10
+            ob_start();             // Ouvrir   la memoire tampon pour contenir lfichier $file à transformer en texte
             require_once($file);
             $content=ob_get_clean();
-            //------------
-            //---Ouvrir à nouveau la memoir tampon pour recevoir le fichier $base ave la variable $content
-            ob_start();
+            ob_start();             // Ouvrir à nouveau la memoire tampon pour recevoir le fichier $base avec la variable $content
             require_once($base);
             $page=ob_get_clean();
             echo $page;
         }else{
-            // cas où le fichier $file n'existe pas
             echo "<h1>Desolé! Le fichier $file n'existe pas!</h1>"; 
             die;
         }
